9c7a4c6930d446f0c21bea9373c069058659a246
JPbooya <perezjcarlos81@gmail.com>
2026-02-10 22:08:50 -0800

    finished problem 1

--- q1.js
+++ q1.js
@@ -1,10 +1,17 @@
 // 1. Calculate the sum of all numbers in an array (using a loop)
 
 function sumArray(array) {
+  let sum = 0;
 
+  array.forEach(function(element) {
+    sum += element;
+  });
+    return sum;
 }
 
 // Test for sum array
 let testArray = [1, 2, 3, 4, 5]; // 15
+
+console.log(sumArray(testArray));
 
 // TODO: display the sum of the array in the console

f39abd2f804a8b21117f4c90afc145a4839ae0ac
JPbooya <perezjcarlos81@gmail.com>
2026-02-10 22:21:39 -0800

    finished problem 2

--- q2.js
+++ q2.js
@@ -1,9 +1,19 @@
 // 2. Find the largest number in an array (using a loop)
 function findLargest(array) {
+  let max = array[0];
 
+   array.forEach(function(element) {
+
+    if (element > max) {
+      max = element;
+    }
+  });
+    return max;
 }
 
 // Test for find largest
 let testArray2 = [1, 4, 5, 4, 3]; // 5
+
+console.log(findLargest(testArray2));
 
 // TODO: display the largest number in the array in the console
